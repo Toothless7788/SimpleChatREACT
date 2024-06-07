eb553adfe50cb13893bbdf068e292f78809648d6
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-06-07 15:49:29 +0100

    feat: establish connection successfully

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+"use client";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3001");    // We will use socket to emit and receive message from server
 
 export default function App() {
+  const sendMessage = () => {
+    // Send a message to server
+    socket.emit("sendMessage", {message: "Hello from user"});    // Name of event, i.e. sendMessage is self-defined -> just need to map the correct one at the server
+  };
+
   return (
-    <h1>Hello World</h1>
+    <div className="flex items-center justify-center mt-5">
+      <div className="border-black border-2">
+        <input placeholder="Type your message" />
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
+    </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,15 +4,23 @@ const http = require("http");
 const {Server} = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors());    // Prevent server from getting connection error due to cross-origin request
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {    // For security checking: define what is allowed
         origin: "http://localhost:3000", 
-        methods: ["GET", "POST"]
-    }
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("sendMessage", (data) => {
+        console.log(`data in server: ${data.message.value}`)
+    })
 });
 
 
