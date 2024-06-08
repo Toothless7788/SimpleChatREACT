c9ff3ebbf107358fdf2b6e5d20d409495a040300
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-06-08 11:36:49 +0100

    feat: add chat room

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,24 +7,67 @@ const socket = io("http://localhost:3001");    // We will use socket to emit and
 export default function App() {
   const [message, setMessage] = useState("");
   const [displayText, setDisplayText] = useState("");
+  const [roomMessage, setRoomMessage] = useState("");
+  const [roomDisplayText, setRoomDisplayText] = useState("");
+  const [room, setRoom] = useState("");
+  
   const sendMessage = () => {
     // Send a message to server
-    socket.emit("sendMessage", {value: message});    // Name of event, i.e. sendMessage is self-defined -> just need to map the correct one at the server
+    socket.emit("send_message", {value: message});    // Name of event, i.e. sendMessage is self-defined -> just need to map the correct one at the server
   };
 
-  socket.on("receiveMessage", (data) => {
+  const sendRoomMessage = () => {
+    if(room != "") {
+      // Send a message for room to server
+      socket.emit("send_room_message", {value: roomMessage, roomID: room})
+    } else {
+      alert("Please input your room ID");
+    }
+  };
+
+  const joinRoom = () => {
+    // Join a room
+    if(room != "") {
+      socket.emit("join_room", room);
+    } else {
+      alert("Please input your room ID");
+    }
+  }
+
+  socket.on("receive_message", (data) => {
     setDisplayText(data["value"]);
   });
 
+  socket.on("receive_room_message", (data) => {
+    setRoomDisplayText(data["value"]);
+  })
+
   return (
-    <div className="flex items-center justify-center mt-5">
-      <div className="border-black border-2">
-        <input className="border-green-700" placeholder="Type your message" onChange={(event) => {
-          setMessage(event.target.value);
-        }}/>
-        <button className="bg-blue-800 hover:bg-blue-300 rounded-xl" onClick={sendMessage}>Send Message</button>
-        <h1>Your message: {displayText}</h1>
+    <div>
+      <div className="flex items-center justify-center mt-5">
+        <div className="border-black border-2">
+          <input className="border-green-700" placeholder="Type your message" onChange={(event) => {
+            setMessage(event.target.value);
+          }}/>
+          <button className="bg-blue-800 hover:bg-blue-300 rounded-xl" onClick={sendMessage}>Send Message</button>
+          <h1>Your message: {displayText}</h1>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-5">
+        <div className="border-black border-2">
+          <input className="border-green-700" placeholder="Type your room ID" onChange={(event) => {
+            setRoom(event.target.value);
+          }}/>
+          <button className="bg-blue-800 hover:bg-blue-300 rounded-xl" onClick={joinRoom}>Join Room</button>
+          <input className="border-green-700" placeholder="Type your room message" onChange={(event) => {
+            setRoomMessage(event.target.value);
+          }}/>
+          <button className="bg-blue-800 hover:bg-blue-300 rounded-xl" onClick={sendRoomMessage}>Send Room Message</button>
+          <h1>Your room message: {roomDisplayText}</h1>
+        </div>
       </div>
     </div>
   );
 }
+
+
--- server/index.js
+++ server/index.js
@@ -16,22 +16,36 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+    /*
+      Summary: 
+      1. socket.broadcase.emit(): Send the data to all clients except the initiator, i.e. sender
+      2. socket.emit(): Send the data to the initiator, i.e. sender
+      3. io.emit(): Send the data to all clients
+      4. socket.join() puts that socket into a room with an ID (opposite: socket.leave())
+      io always refers to all clients while socket always refers to 1 clients who is the initiator, i.e. sender
+      Notes: 
+      - socket.to().emit() is related to io.in().emit()
+    */
     console.log(`User connected: ${socket.id}`);
 
-    socket.on("sendMessage", (data) => {
+    // For single connection
+    socket.on("send_message", (data) => {
         console.log(`data in server: ${data["value"]}`)    // data["message"] is the same as data.message
 
-        /*
-          Summary: 
-          1. socket.broadcase.emit(): Send the data to all clients except the initiator, i.e. sender
-          2. socket.emit(): Send the data to the initiator, i.e. sender
-          3. io.emit(): Send the data to all clients
-        */
         // socket.broadcast.emit("receiveMessage", data);    // Send to all clients except sender
         // socket.emit("receiveMessage", data);    // Send to sender only
-        io.emit("receiveMessage", data);    // Send to all clients
+        io.emit("receive_message", data);    // Send to all clients
         
+    });
+
+    // For room connection
+    socket.on("join_room", (roomID) => {
+        socket.join(roomID);
     })
+
+    socket.on("send_room_message", (roomMessage) => {
+        socket.to(roomMessage.roomID).emit("receive_room_message", roomMessage);
+    });
 });
 
 
